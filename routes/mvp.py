d2f787e76ce0f816b17057142a7efab3bae64533
Aman Ibrahim <amanmibra@gmail.com>
2023-06-01 13:42:42 -0400

    Add labels endpoint

--- routes/mvp.py
+++ routes/mvp.py
@@ -5,11 +5,67 @@ from firebase_admin import firestore
 
 # utils
 from utils.firebase import init_firebase
+from utils.score import process_and_score
 
 mvp = Blueprint("mvp", __name__)
 
 db = init_firebase()
-collection = db.collection("test")
+
+users = db.collection("users")
+
+
+@mvp.route("/label/add", methods=["PUT", "POST"])
+def add_urls_to_label():
+    urls = request.json.get("urls")
+    label = request.json.get("label")
+    print(f"urls {urls[0]}, label {label}")
+
+    if not (urls and label):
+        return jsonify(code=400, message="Missing urls or label in request body.")
+
+    # if isinstance(urls, list):
+    #     return jsonify(code=400, message="urls is expected to be a list.")
+
+    data = dict(urls=urls)
+    try:
+        if request.method == "PUT":
+            # create or replace - PUT
+            users.document(label).set(data)
+        else:
+            # update - POST
+            doc_ref = users.document(label).get()
+            if doc_ref.exists:
+                current_urls = doc_ref.get("urls")
+                combined_urls = list(set(current_urls).union(set(urls)))
+                data = dict(urls=combined_urls)
+            users.document(label).set(data, merge=True)
+    except Exception as e:
+        return jsonify(
+            code=500,
+            message="Error caught",
+            error=str(e),
+        )
+
+    return jsonify(code=201, message="Data created successfully!")
+
+
+@mvp.route("/score", methods=["GET"])
+def score():
+    url = request.args.get("url")
+    label = request.args.get("label")
+
+    if not url or not label:
+        return jsonify(code=400, message="Missing url or label in request body.")
+
+    test_url = "gs://deeptruth-fb46f.appspot.com/aman_1.wav"
+
+    return jsonify(code=200, data=process_and_score(url, test_url))
+
+
+"""
+Test Routes
+"""
+test_collection = db.collection("test")
 
 
 @mvp.route("/test", methods=["GET"])
@@ -20,7 +76,7 @@ def test_route():
 @mvp.route("/test/<id>", methods=["GET"])
 def get_test(id):
     try:
-        data = collection.document(id).get()
+        data = test_collection.document(id).get()
 
         return jsonify(
             code=200,
@@ -44,10 +100,10 @@ def update_test():
     try:
         if request.method == "PUT":
             # create or replace - PUT
-            collection.document(id).set(data)
+            test_collection.document(id).set(data)
         else:
             # update - POST
-            collection.document(id).set(data, merge=True)
+            test_collection.document(id).set(data, merge=True)
     except Exception as e:
         return jsonify(
             code=500,
